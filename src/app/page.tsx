ab1d0cac12180224a86f1d9be91594f167041a28
0xkenop <153365368+0xkenop@users.noreply.github.com>
2023-12-11 17:59:40 +0800

    display api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import CenteredContainer from "../components/CenteredContainer";
 import { ConnectButton } from "../components/ConnectButton";
 import ProgressBar from "../components/ProgressBar";
@@ -11,7 +11,19 @@ import MobileHidden from "../components/MobileHidden";
 import Preloader from "../components/preloader/Preloader";
 import Socials from "../components/Socials";
 import "../styles/styles.css";
+import Decimal from "decimal.js";
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [t1, setT1] = useState("0")
+  useEffect(() => {
+    fetch("https://eorc20.com/api/tickOpHistory/eoss/minted/progress")
+      .then(res => res.json().then(res => {
+        const {mintMax, minted} = res?.data
+        setT1(new Decimal(minted).div(new Decimal(mintMax)).mul(new Decimal(100)).toFixed(2))
+      }))
+  }, [])
+
   return (
     <>
       <Preloader />
@@ -28,9 +40,9 @@ function Home() {
         </div>
       </div>
       <CenteredContainer>
-        <ProgressBar percent={88.8} />
+        <ProgressBar percent={t1} />
         <h2 className="scriberTitle">EOS EORCscriber</h2>
-        <div className="scriberTitle1">A tool to inscrib EORC20</div>
+        <div className="scriberTitle1">A tool to inscribe EORC20</div>
         <Ethscribe />
         <MyAssets />
       </CenteredContainer>
--- src/components/MyAssets.tsx
+++ src/components/MyAssets.tsx
@@ -1,21 +1,55 @@
 import React, { ReactNode } from "react";
 import { Pagination } from "@mui/material";
 import { ChangeEvent, useCallback, useEffect, useState } from "react";
+import { useAccount } from "wagmi";
 
 interface CenteredContainerProps {
   children: ReactNode;
 }
 
 function MyAssets({ children }: any) {
-  const assets: any = [{}, {}, {}, {}, {}];
+  const [assets, setAssets] = useState([])
 
   const [currentPage, setCurrentPage] = useState(1);
   const assetsPerPage = 10; // 每页显示的用户数量
   // 计算总页数
-  const totalPages = Math.ceil(assets.length / assetsPerPage);
+  const [totalPages, setTotalPages] = useState(1)
 
+  const { isConnected } = useAccount();
+
+  const account = useAccount();
+  useEffect(() => {
+    if (isConnected) {
+      if (account.address !== undefined) {
+        fetch(`https://eorc20.com/api/tickOpHistory/page?pageNumber=1&pageSize=${assetsPerPage}&fromAddress=${account.address}&tick=eoss&opType=mint`).then(res => {
+          console.log(res)
+          res.json().then(res => {
+            console.log(res)
+            const {total, opList} = res?.data
+            console.log(opList)
+            setAssets(opList)
+            setTotalPages(Math.ceil(total / assetsPerPage))
+          })
+        })
+      }
+    }
+  },[])
+  
   // 处理页码变更
   const handlePageChange = (event: any, value: any) => {
+    if (isConnected) {
+      if (account.address !== undefined) {
+        console.log(event, value)
+        fetch(`https://eorc20.com/api/tickOpHistory/page?pageNumber=${value}&pageSize=${assetsPerPage}&fromAddress=${account.address}&tick=eoss&opType=mint`).then(res => {
+          res.json().then(res => {
+            const {total, opList} = res?.data
+            console.log(opList)
+            setAssets(opList)
+            setTotalPages(Math.ceil(total / assetsPerPage))
+          })
+        })
+      }
+    }
     setCurrentPage(value);
   };
   return (
@@ -24,12 +58,11 @@ function MyAssets({ children }: any) {
       <div className="assetsList">
         {assets.map((item: any, index: number) => {
           return (
-            <div className="assetsLi" key={index}>{`{
-     "p":"eorc20",
-     "op":"mint",
-     "tick":"eoss",
-     "amt":"10000"
-}`}</div>
+            <div className="assetsLi" key={index}>
+              {
+                item?.opJson
+              }
+            </div>
           );
         })}
       </div>
