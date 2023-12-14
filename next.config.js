fe3d11470c29c9e108af7a4241c8da6ccaf5dd4b
yao <594861106@qq.com>
2023-12-15 00:36:21 +0800

    tijiao

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+    serverFileSystem: false,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     return config
